15245375303850b7f68c82a703d948f9086163e4
Long Ho <holevietlong@gmail.com>
2026-05-18 22:03:25 -0400

    chore(release): prepare codescythe cli v0.3.1

--- crates/codescythe_cli/e2e.rs
+++ crates/codescythe_cli/e2e.rs
@@ -21,7 +21,7 @@ fn cli_reports_release_version() {
     );
     assert_eq!(
         String::from_utf8_lossy(&output.stdout).trim(),
-        "codescythe 0.3.0"
+        "codescythe 0.3.1"
     );
 }
 
